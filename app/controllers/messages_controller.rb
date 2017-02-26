53ad3da43d72c79ab979049bbcf2f6d69b68298e
Kevin Finley <kftwotwo@gmail.com>
2017-02-25 19:43:12 -0800

    add the views for messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,30 @@
 class MessagesController < ApplicationController
+
+  def index
+    @messages = Message.all
+  end
+
+  def show
+      @message = Message.find(params[:id])
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      flash[:notice] = "Message was sent!"
+      redirect_to messages_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def message_params
+    params.requrie(:message).premit(:to, :from, :body)
+  end
 end
